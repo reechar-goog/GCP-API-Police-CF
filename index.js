82b0ab8183bb0f9c481af197928c9b6cef96fbc2
Richard Hsu <reechar@google.com>
2018-09-14 17:28:43 -0400

    more respectful code and error handling

--- index.js
+++ index.js
@@ -26,17 +26,23 @@ exports.apiPolice = (event, callback) => {
   const blockedList = ['translate.googleapis.com']
 
   console.log(`${accountID} attempted to activate ${apiName} in ${projectID}`)
-
   if (blockedList.indexOf(apiName) > -1) {
     console.error(`${apiName} is on the blocked API list and will be disabled`);
-    servicemanagement.services.disable({ auth: authenticationClient, serviceName: apiName, requestBody: { consumerId: projectID } });
+    servicemanagement.services.disable({ auth: authenticationClient, serviceName: apiName, requestBody: { consumerId: projectID } })
+      .then(function (response) {
+        callback(null, 'Success!');
+      })
+      .catch(function (error) {
+        callback(new Error('Failed'));
+      });
   } else {
     console.log(`${apiName} not blocked`);
+    callback(null, 'Success!');
   }
-  callback();
+
 }
 
-exports.apiPoliceWhiteList = (event, callback) => {
+exports.apiPoliceAllowedList = (event, callback) => {
   const pubsubMessage = event.data;
   const cloudAuditLogMsg = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString())
 
@@ -44,7 +50,7 @@ exports.apiPoliceWhiteList = (event, callback) => {
   const apiName = cloudAuditLogMsg.protoPayload.request.serviceName
   const projectID = "project:" + cloudAuditLogMsg.resource.labels.project_id
 
-  const whitelist = ['bigquery-json.googleapis.com',
+  const allowedList = ['bigquery-json.googleapis.com',
     'cloudapis.googleapis.com',
     'clouddebugger.googleapis.com',
     'cloudfunctions.googleapis.com',
@@ -61,15 +67,21 @@ exports.apiPoliceWhiteList = (event, callback) => {
     'sql-component.googleapis.com',
     'storage-api.googleapis.com',
     'storage-component.googleapis.com',
-    'vision.googleapis.com'] //vision.googleapis.com not part of default enable APIs, here for exmaple only
+    'vision.googleapis.com'] //vision.googleapis.com not part of default enable APIs, here for example only
 
   console.log(`${accountID} attempted to activate ${apiName} in ${projectID}`)
-  if (whitelist.indexOf(apiName) > -1) {
-    console.log(`${apiName} is whitelisted`);
+  if (allowedList.indexOf(apiName) > -1) {
+    console.log(`${apiName} is in the allowed list`);
+    callback(null, 'Success!');
   } else {
-    console.error(`${apiName} not in whitelist and will be disabled`);
-    servicemanagement.services.disable({ auth: authenticationClient, serviceName: apiName, requestBody: { consumerId: projectID } });
+    console.error(`${apiName} not in allowed list and will be disabled`);
+    servicemanagement.services.disable({ auth: authenticationClient, serviceName: apiName, requestBody: { consumerId: projectID } })
+      .then(function (response) {
+        callback(null, 'Success!');
+      })
+      .catch(function (error) {
+        callback(new Error('Failed'));
+      });
   }
-  callback();
 }
 
